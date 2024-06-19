3560c675e79427db7acbc780b888ca548d521a76
Ramesh <jeniramesh142@gmail.com>
2024-06-19 17:14:10 -0400

    Add project files.

---
+++ LaptopDiscount/Discount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopDiscount
+{
+    public enum EmployeeType
+    {
+        PartTime,       // No Discount
+        PartialLoad,    // 7% Discount
+        FullTime,       // 12% Discount
+        CompanyPurchasing // 22% Discount
+    }
+
+    public class EmployeeDiscount
+    {
+        public EmployeeType EmployeeType { get; set; }
+        public decimal Price { get; set; }
+        public object Assert { get; set; }
+
+        public decimal CalculateDiscountedPrice()
+        {
+            decimal discount = 0;
+
+            switch (EmployeeType)
+            {
+                case EmployeeType.PartTime:
+                    discount = 0; // No discount
+                    break;
+                case EmployeeType.PartialLoad:
+                    discount = 0.07m; // 5% discount
+                    break;
+                case EmployeeType.FullTime:
+                    discount = 0.12m; // 10% discount
+                    break;
+                case EmployeeType.CompanyPurchasing:
+                    discount = 0.22m; // 20% discount
+                    break;
+            }
+
+            return Price - (Price * discount);
+        }
+    }
+
+}
---
+++ ProductTest/TestProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductTest
+{
+    public class TestProduct
+    {
+        [Test]
+        public void ProductConstructor_ValidInputRedmi14_SetsProductIDCorrectly()
+        {
+            //Arrange
+            int productID = 6;
+            string productName = "Redmi 14";
+            int price = 650;
+            int stock = 7;
+
+            int expected = 6;
+
+            //Act
+            Product product = new Product(productID, productName, price, stock);
+            int actual = product.ProductID;
+
+            //Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+    }
+}
